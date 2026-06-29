8d6e674760ec20a2ba542f6190b6132dfaf853dc
Daniel Åvall <daniel@avall.net>
2026-06-29 10:12:41 +0200

    Moved large font to separate file

--- app/main.py
+++ app/main.py
@@ -1,48 +1,8 @@
 import time
 from epd2in9 import EPD_2in9_Portrait
+from large_font import draw_large_text
 
-use_knots = False
-
-font = {
-    '0': ("01110", "10001", "10011", "10101", "11001", "10001", "01110"),
-    '1': ("00100", "01100", "00100", "00100", "00100", "00100", "01110"),
-    '2': ("01110", "10001", "00001", "00110", "01000", "10000", "11111"),
-    '3': ("01110", "10001", "00001", "00110", "00001", "10001", "01110"),
-    '4': ("00010", "00110", "01010", "10010", "11111", "00010", "00010"),
-    '5': ("11111", "10000", "11110", "00001", "00001", "10001", "01110"),
-    '6': ("00110", "01000", "10000", "11110", "10001", "10001", "01110"),
-    '7': ("11111", "00001", "00010", "00100", "01000", "01000", "01000"),
-    '8': ("01110", "10001", "10001", "01110", "10001", "10001", "01110"),
-    '9': ("01110", "10001", "10001", "01111", "00001", "00010", "01100"),
-    ':': ("00000", "00100", "00000", "00000", "00100", "00000", "00000"),
-    '-': ("00000", "00000", "00000", "11111", "00000", "00000", "00000"),
-    '.': ("00000", "00000", "00000", "00000", "00000", "00100", "00100"),
-    ' ': ("00000", "00000", "00000", "00000", "00000", "00000", "00000"),
-}
-
-def draw_large_text(epd, text, x, y, scale=2):
-    font_color = epd.black
-    background_color = epd.white
-
-    # clear background
-    epd.fill_rect(x, y, len(str(text)) * (6 * scale), 7 * scale, background_color)
-
-    # draw text
-    for index, char in enumerate(str(text)):
-        glyph = font.get(char)
-        if glyph is None:
-            continue
-
-        for row, line in enumerate(glyph):
-            for col, pixel in enumerate(line):
-                if pixel == '1':
-                    epd.fill_rect(
-                        x + index * (6 * scale) + col * scale,
-                        y + row * scale,
-                        scale,
-                        scale,
-                        font_color,
-                    )
+use_knots = True  # True for knots, False for km/h
 
 
 if __name__=='__main__':
@@ -76,7 +36,7 @@ if __name__=='__main__':
     draw_large_text(epd, '--:--', x_value, y_value + 3*y_space, scale=4)
 
     epd.text("Clock", x_title, y_title + 4*y_space, epd.black)
-    draw_large_text(epd, '--:--', x_value, y_value + 4*y_space, scale=4)
+    draw_large_text(epd, '12:34', x_value, y_value + 4*y_space, scale=4)
     epd.display(epd.buffer)
 
     for i in range(0, 5):
@@ -88,39 +48,8 @@ if __name__=='__main__':
 
         epd.delay_ms(1000)
     
-    #epd.vline(10, 90, 60, 0x00)
-    #epd.vline(120, 90, 60, 0x00)
-    #epd.hline(10, 90, 110, 0x00)
-    #epd.hline(10, 150, 110, 0x00)
-    #epd.line(10, 90, 120, 150, 0x00)
-    #epd.line(120, 90, 10, 150, 0x00)
-    #epd.display(epd.buffer)
-    #epd.delay_ms(2000)
+    draw_large_text(epd, '--:--', x_value, y_value + 3*y_space, scale=4)
+    epd.display_Partial(epd.buffer)    
     
-    #epd.rect(10, 180, 50, 80, 0x00)
-    #epd.fill_rect(70, 180, 50, 80, 0x00)
-    #epd.display_Base(epd.buffer)
-    #epd.delay_ms(2000)
-    
-    #for i in range(0, 10):
-    #    epd.fill_rect(40, 270, 40, 10, 0xff)
-    #    epd.text(str(i), 60, 270, 0x00)
-    #    epd.display_Partial(epd.buffer)
-
-    #epd.init_4Gray()
-    #epd.image4Gray.fill_rect(0, 0, 127, 74, epd.black)  
-    #epd.image4Gray.text('GRAY1',10, 33, epd.white)
-    #epd.image4Gray.fill_rect(0, 74, 127, 74, epd.darkgray)
-    #epd.image4Gray.text('GRAY2',10, 107, epd.grayish)
-    #epd.image4Gray.fill_rect(0, 148, 127, 74, epd.grayish)
-    #epd.image4Gray.text('GRAY3',10, 181, epd.darkgray)
-    #epd.image4Gray.fill_rect(0, 222, 127, 74, epd.white)
-    #epd.image4Gray.text('GRAY4',10, 255, epd.black)
-    #epd.display_4Gray(epd.buffer_4Gray)
-    #epd.delay_ms(5000)
-
-    #epd.init()
-    #epd.Clear(0xff)
-    #epd.delay_ms(2000)
     print("sleep")
     epd.sleep()
